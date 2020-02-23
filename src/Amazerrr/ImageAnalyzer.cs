55bb6b315e59e6cbf41034b93dfd72f1df26268b
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-23 20:25:56 +0200

    Add image analyzer placeholders

---
+++ src/Amazerrr/ImageAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazerrr
+{
+    public class ImageAnalyzer
+    {
+        public string Analyze(byte[] imageData)
+        {
+            return null;
+        }
+    }
+}
---
+++ test/unit/Amazerrr.Tests/ImageAnalyzerTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Amazerrr.Tests
+{
+    public class ImageAnalyzerTests
+    {
+        private readonly ImageAnalyzer _imageAnalyzer;
+
+        public ImageAnalyzerTests()
+        {
+            _imageAnalyzer = new ImageAnalyzer();
+        }
+
+        [Fact]
+        public void Analyze_Test1_File()
+        {
+            // Arrange
+            var expected =
+                "######\n" +
+                "#....#\n" +
+                "####.#\n" +
+                "#o...#\n" +
+                "######";
+
+            // Act
+            var actual = _imageAnalyzer.Analyze(Resources.Test1);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
