4f559d4e124c6e9c8f274302fe06fb4b1d3c5a79
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-25 23:25:43 +0200

    Add solver API

--- src/AmazerrrFunc/ImageAnalyzerFunction.cs
+++ src/AmazerrrFunc/ImageAnalyzerFunction.cs
@@ -1,10 +1,10 @@
-using System.IO;
+using Amazerrr;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Amazerrr;
+using System.IO;
 
 namespace AmazerrrFunc
 {
---
+++ src/AmazerrrFunc/SolverFunction.cs
@@ -0,0 +1,33 @@
+using Amazerrr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AmazerrrFunc
+{
+    public static class SolverFunction
+    {
+        [FunctionName("Solver")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            using var reader = new StreamReader(req.Body);
+            var input = await reader.ReadToEndAsync();
+
+            var parser = new Parser();
+            var board = parser.Parse(input);
+
+            var solver = new Solver();
+            var output = solver.Solve(board);
+
+            return new OkObjectResult(output);
+        }
+    }
+}
