869cdde8518fd1db8e9e74d53c9d090fee2f2ea8
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-25 23:13:17 +0200

    Add Azure Functions project

---
+++ src/AmazerrrFunc/ImageAnalyzerFunction.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Amazerrr;
+
+namespace AmazerrrFunc
+{
+    public static class ImageAnalyzerFunction
+    {
+        [FunctionName("ImageAnalyzer")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (!req.ContentLength.HasValue)
+            {
+                return new BadRequestObjectResult("Content-Length is required header.");
+            }
+
+            const int maxSize = 10_000_000;
+            if (req.ContentLength.Value > maxSize)
+            {
+                return new BadRequestObjectResult("Too large content. Try with smaller image.");
+            }
+            
+            using var reader = new BinaryReader(req.Body);
+            var imageData = reader.ReadBytes((int)req.ContentLength.Value);
+
+            var imageAnalyzer = new ImageAnalyzer();
+            var output = imageAnalyzer.Analyze(imageData);
+
+            return new OkObjectResult(output);
+        }
+    }
+}
