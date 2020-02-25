82fb81d97c98aaae39d6cf341ccc07e3abe8c890
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-25 20:06:36 +0200

    Implement parser

--- src/Amazerrr/Board.cs
+++ src/Amazerrr/Board.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace Amazerrr
 {
     public class Board
     {
         public int TotalCount { get; set; }
+        public Point StartPosition { get; set; }
         public Dictionary<string, List<Move>> Moves { get; set; }
 
         public Board()
--- src/Amazerrr/Parser.cs
+++ src/Amazerrr/Parser.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Amazerrr
 {
     public class Parser
     {
         private Board _board = new Board();
+        private HashSet<string> _positions = new HashSet<string>();
+        private HashSet<string> _scannedPaths = new HashSet<string>();
         private char[,] _map;
         private int _height;
         private int _width;
@@ -13,16 +17,75 @@ namespace Amazerrr
         {
             var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             (int x, int y) = FindPlayer(lines);
+            _board.StartPosition = new Point(x, y);
 
-            FindMoves(x, y);
+            FindMoves(x, y, -1, 0);
+            FindMoves(x, y, 1, 0);
+            FindMoves(x, y, 0, -1);
+            FindMoves(x, y, 0, 1);
+
+            _board.TotalCount = _positions.Count;
             return _board;
         }
 
-        private void FindMoves(int x, int y)
+        private void FindMoves(int x, int y, int xdelta, int ydelta)
         {
-            var c = _map[x, y];
+            var startx = x;
+            var starty = y;
+            var startkey = $"{x},{y}";
+            var scankey = $"{x},{y},{xdelta},{ydelta}";
+            if (_scannedPaths.Contains(scankey))
+            {
+                // We have previously already scanned to this direction.
+                return;
+            }
 
-            // TODO: Add available moves to board.
+            _scannedPaths.Add(scankey);
+            while (true)
+            {
+                var newx = x + xdelta;
+                var newy = y + ydelta;
+
+                var c = _map[newx, newy];
+                if (c == Constants.Wall)
+                {
+                    if (startx == x && starty == y)
+                    {
+                        // No move available in this direction.
+                        return;
+                    }
+
+                    var move = new Move()
+                    {
+                        From = new Point(startx, starty),
+                        To = new Point(x, y)
+                    };
+
+                    if (_board.Moves.ContainsKey(startkey))
+                    {
+                        _board.Moves[startkey].Add(move);
+                    }
+                    else
+                    {
+                        _board.Moves.Add(startkey, new List<Move>()
+                        {
+                            move
+                        });
+                    }
+
+                    FindMoves(x, y, -1, 0);
+                    FindMoves(x, y, 1, 0);
+                    FindMoves(x, y, 0, -1);
+                    FindMoves(x, y, 0, 1);
+                    return;
+                }
+
+                var key = $"{x},{y}";
+                _positions.Add(key);
+
+                x = newx;
+                y = newy;
+            }
         }
 
         private (int, int) FindPlayer(string[] lines)
