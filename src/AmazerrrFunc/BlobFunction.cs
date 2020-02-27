d243ef99b13f29519e644daf2f66e893739e644a
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-27 22:24:22 +0200

    Add blob function

---
+++ src/AmazerrrFunc/BlobFunction.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AmazerrrFunc
+{
+    public static class BlobFunction
+    {
+        [FunctionName("BlobFunction")]
+        public static void Run([BlobTrigger("puzzles/{name}", Connection = "Storage")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
