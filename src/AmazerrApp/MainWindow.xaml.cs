1e7f064c42b3c4c9c912045ead9cd21785b979ce
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-27 10:25:30 +0200

    Add simple UI

--- src/AmazerrApp/MainWindow.xaml.cs
+++ src/AmazerrApp/MainWindow.xaml.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Win32;
 
 namespace AmazerrApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,7 +12,16 @@ namespace AmazerrApp
 
         private void SolveButton_Click(object sender, RoutedEventArgs e)
         {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files (*.png)|*.PNG|All files (*.*)|*.*"
+            };
 
+            var result = openFileDialog.ShowDialog();
+            if (result.GetValueOrDefault())
+            {
+                Swipes.Text = openFileDialog.FileName;
+            }
         }
     }
 }
