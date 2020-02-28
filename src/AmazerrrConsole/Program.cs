1aafe8c6b55c2e916b959cf617fb036bf9a4b445
Janne Mattila <janne.mattila@hotmail.fi>
2020-02-28 14:37:20 +0200

    Add console project

---
+++ src/AmazerrrConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazerrr;
+
+namespace AmazerrrConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Amazerrr");
+            if (args.Length == 1)
+            {
+                var imageData = await File.ReadAllBytesAsync(args[0]);
+
+                var imageAnalyzer = new ImageAnalyzer();
+                var input = imageAnalyzer.Analyze(imageData);
+
+                Console.WriteLine("Board:");
+                Console.WriteLine(input);
+                Console.WriteLine();
+
+                var parser = new Parser();
+                var board = parser.Parse(input);
+
+                var solver = new Solver();
+                var output = solver.Solve(board);
+
+                Console.WriteLine($"Solution in {output.Count} moves:");
+                foreach (var item in output)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+    }
+}
